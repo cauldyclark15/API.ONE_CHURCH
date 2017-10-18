c85ecb52f7959ee5b82ce14069a056479413b6fb
cauldyclark15 <jcing.jc@gmail.com>
2017-10-18 17:11:29 +0800

    Create mock graphql schema

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+import bodyParser from 'body-parser';
+import { createServer } from 'http';
+import cors from 'cors';
+
+import Schema from './data/schema';
+import Mocks from './data/mocks';
+
+const GRAPHQL_PORT = process.env.PORT || 7000;
+const app = express();
+
+const executableSchema = makeExecutableSchema({
+  typeDefs: Schema,
+});
+
+addMockFunctionsToSchema({
+  schema: executableSchema,
+  mocks: Mocks,
+  preserveResolvers: true,
+});
+
+app.use(cors());
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema: executableSchema,
+  context: {},
+}));
+
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
+}));
+
+const graphQLServer = createServer(app);
+
+graphQLServer.listen(GRAPHQL_PORT, () => {
+  console.log(`Server running at port: ${GRAPHQL_PORT}`);
+});
